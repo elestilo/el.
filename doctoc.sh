09e099b9eb07f0189e9ccac2c9553ca76da87f8d
Claudio Sanches <contato@claudiosanches.com>
2019-04-16 16:30:36 -0300

    Added messages about requirements

--- doctoc.sh
+++ doctoc.sh
@@ -5,4 +5,14 @@
 # npm install -g doctoc
 # Ack: https://beyondgrep.com/documentation/
 
+command -v doctoc >/dev/null 2>&1 || { 
+  echo >&2 "doctoc is required! Install with \"npm install -g doctoc\". Aborting...";
+  exit 1;
+}
+
+command -v doctoc >/dev/null 2>&1 || { 
+  echo >&2 "ack is required! Aborting...";
+  exit 1;
+}
+
 ack -L 'DOCTOC SKIP' | xargs doctoc *.md --title '## Table of Contents' --github 
